d184c8439356d8c828dba9e61ac07f93a78d8ed5
michael@speicherleck.de <michael@speicherleck.de@7f39b8f8-30bc-11de-9e80-b52b74bdfc52>
2009-06-18 16:16:14 +0000

    mv

--- firmware/secstick_v1/openkubus.c
+++ firmware/secstick_v1/openkubus.c
@@ -1,3 +1,21 @@
+/*
+ * (c) 2009, Benedikt Sauter, Michael Hartmann
+ * original taken from usbprog source
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define F_CPU 16000000UL  // 16 MHz
 
 #include <avr/interrupt.h>
@@ -8,7 +26,7 @@
 #include <string.h>
 #include <util/delay.h>
 
-#include "main.h"
+#include "openkubus.h"
 #include "../addresses.h"
 #include "base64.h"
 #include "eeprom.h"

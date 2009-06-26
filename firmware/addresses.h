b0aa59b288ec51f7ab6c833f036689199b1412ac
michael@speicherleck.de <michael@speicherleck.de@7f39b8f8-30bc-11de-9e80-b52b74bdfc52>
2009-06-26 14:48:57 +0000

    .

--- firmware/addresses.h
+++ firmware/addresses.h
@@ -1,8 +1,20 @@
-#define ADDR_LOCK        10
-#define ADDR_CONF        11
-#define ADDR_COUNTER     16
-#define ADDR_KEY         20
-#define ADDR_DATA        60
-#define USB_WRITE_EEPROM 30
-#define USB_CHANGE_KEY   8
-#define ADDR_SEED        20
+#define ADDR_LOCK        1  // [1-1]:    1 byte
+#define ADDR_TIMESTAMP   2  // [2-5]:    4 bytes; Y2K38 bug
+#define ADDR_ID          6  // [6-9]:    4 bytes
+#define ADDR_COMPANY     10 // [10-40]:  30 chars + NUL
+#define ADDR_OWNER       41 // [41-72]:  30 chars + NUL
+#define ADDR_COUNTER     73 // [73-74]:  2 bytes
+#define ADDR_SEED        75 // [75-122]: 48 bytes
+
+#define USB_CHANGE_KEY    20
+#define USB_GET_COMPANY   21
+#define USB_GET_ID        22
+#define USB_GET_OWNER     23
+#define USB_GET_TIMESTAMP 24
+#define USB_WRITE_EEPROM  25
+
+#define LEN_COMPANY       31
+#define LEN_ID            4
+#define LEN_OWNER         31
+#define LEN_TIMESTAMP     4
+#define LEN_SEED          46
